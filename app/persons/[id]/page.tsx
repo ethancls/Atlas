9414ab7648c3acaa8416ed2273d0108198b2b585
Dimitri <dmytro.palahin@gmail.com>
2024-11-20 19:26:31 +0100

    change style

--- app/persons/[id]/page.tsx
+++ app/persons/[id]/page.tsx
@@ -56,7 +56,7 @@ const PersonDetailPage = ({ params }: { params: { id: string } }) => {
   const [person, setPerson] = useState<Person | null>(null);
   const [Movies, setMovies] = useState<Movies[]>([]);
   const [TVShows, setTVShows] = useState<TVShows[]>([]);
-  const [showFullBiography, setShowFullBiography] = useState(false);
+  const [showFullBiography] = useState(false);
 
   useEffect(() => {
     const fetchPersonDetails = async () => {
@@ -111,18 +111,20 @@ const PersonDetailPage = ({ params }: { params: { id: string } }) => {
   return (
     <div className="relative flex min-h-screen min-w-full bg-cover">
       {/* Content */}
-      <div className="flex-1 flex flex-col gap-4 backdrop-blur-2xl bg-slate-800/40 p-6 lg:p-12 overflow-hidden">
-        {/* Bouton de retour */}
-        <button
-          onClick={() => router.back()}
-          className="absolute top-4 left-4 z-30 flex items-center gap-2 p-2"
-          title="Go back"
-        >
-          <ChevronLeftIcon className="h-7 w-7 text-white" />
-          <span className="text-white font-medium"></span>
-        </button>
+      <div className="flex-1 flex flex-col gap-4 backdrop-blur-2xl pt-2 px-18 pb-18 lg:pt-6 lg:px-20 lg:pb-20 overflow-hidden">
+        {/* Back Button */}
+        <div className="absolute top-4 left-6 z-30 p-0 rounded-lg shadow-md flex items-center justify-center bg-gray-100 dark:bg-[rgb(24,24,27)]">
+          <button
+            onClick={() => router.back()}
+            className="flex items-center justify-center w-full h-full"
+            title="Go back"
+          >
+            <ChevronLeftIcon className="h-7 w-7 text-gray-400" />
+            <span className="text-gray-400 font-medium"></span>
+          </button>
+        </div>
 
-        <div className="flex flex-col items-center md:flex-row gap-6 lg:gap-8 bg-slate-700/50 p-4 rounded-lg shadow-md">
+        <div className="flex flex-col items-center md:flex-row gap-6 lg:gap-8 p-4 rounded-lg shadow-md shadow-black/30 bg-gradient-to-b from-gray-100 to-gray-200 dark:from-[rgb(24,24,27)] dark:to-[rgb(48,48,51)]">
           {/* Profile Picture */}
           <div className="flex-shrink-0 mx-auto md:mx-0 w-[50%] flex justify-center relative">
             <button
@@ -134,7 +136,7 @@ const PersonDetailPage = ({ params }: { params: { id: string } }) => {
 
                 const img = document.createElement('img');
                 img.src = `https://image.tmdb.org/t/p/original${person.profile_path}`;
-                img.className = 'max-h-[80vh] max-w-[80vw] object-contain rounded-lg shadow-lg';
+                img.className = 'max-h-[80vh] max-w-[80vw] object-contain rounded-lg shadow-lg shadow-black/50';
 
                 modal.appendChild(img);
                 document.body.appendChild(modal);
@@ -150,7 +152,7 @@ const PersonDetailPage = ({ params }: { params: { id: string } }) => {
 
                   const img = document.createElement('img');
                   img.src = `https://image.tmdb.org/t/p/original${person.profile_path}`;
-                  img.className = 'max-h-[80vh] max-w-[80vw] object-contain rounded-lg shadow-lg';
+                  img.className = 'max-h-[80vh] max-w-[80vw] object-contain rounded-lg shadow-lg shadow-black/50';
 
                   modal.appendChild(img);
                   document.body.appendChild(modal);
@@ -166,7 +168,7 @@ const PersonDetailPage = ({ params }: { params: { id: string } }) => {
                 width={300}
                 height={450}
                 quality={100}
-                className="rounded-full shadow-lg w-[150px] md:w-[200px] lg:w-[300px] h-auto duration-300 ease-out hover:scale-105"
+                className="rounded-full shadow-lg shadow-black/50 w-[150px] md:w-[200px] lg:w-[300px] h-auto duration-300 ease-out hover:scale-105"
               />
             </button>
           </div>
@@ -179,26 +181,58 @@ const PersonDetailPage = ({ params }: { params: { id: string } }) => {
               {person.biography.length > 400 && (
                 <button
                   onClick={() => {
-                    if (!showFullBiography) {
-                      const modal = document.createElement('div');
-                      modal.className = 'fixed inset-0 z-50 flex items-center justify-center bg-black/80 p-4';
-                      modal.onclick = () => modal.remove();
+                    const modal = document.createElement('div');
+                    modal.className = 'fixed inset-0 z-50 flex items-center justify-center bg-black/80 p-4';
 
-                      const bio = document.createElement('p');
-                      bio.className = 'text-white max-w-[80vw] max-h-[80vh] overflow-auto';
-                      bio.innerText = person.biography;
+                    // Close modal function
+                    const closeModal = () => {
+                      modal.remove();
+                    };
 
-                      modal.appendChild(bio);
-                      document.body.appendChild(modal);
+                    const modalContent = document.createElement('div');
+                    modalContent.className =
+                      'bg-gray-200 rounded-lg max-w-[80vw] max-h-[80vh] overflow-auto flex flex-col shadow-lg shadow-black/50';
 
-                      addEscapeKeyListener(modal);
-                    } else {
-                      setShowFullBiography(false);
-                    }
+                    // Header with name
+                    const header = document.createElement('h2');
+                    header.className = 'mt-4 text-black text-2xl font-bold text-center';
+                    header.innerText = person.name;
+                    // Divider line
+                    const divider = document.createElement('hr');
+                    divider.className = 'border-t border-gray-400 my-3';
+
+                    // Biography text
+                    const bio = document.createElement('p');
+                    bio.className = 'text-black text-lg mb-2 p-6';
+                    bio.innerText = person.biography;
+
+                    // "Done" button
+                    const doneButton = document.createElement('button');
+                    doneButton.className =
+                      'px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition duration-300 self-end mb-6 mr-8';
+                    doneButton.innerText = 'Done';
+                    doneButton.onclick = closeModal;
+
+                    modalContent.appendChild(header);
+                    modalContent.appendChild(divider);
+                    modalContent.appendChild(bio);
+                    modalContent.appendChild(doneButton);
+                    modal.appendChild(modalContent);
+                    document.body.appendChild(modal);
+
+                    // Close modal with Escape key
+                    const handleKeyDown = (e: KeyboardEvent) => {
+                      if (e.key === 'Escape') {
+                        closeModal();
+                        document.removeEventListener('keydown', handleKeyDown);
+                      }
+                    };
+
+                    document.addEventListener('keydown', handleKeyDown);
                   }}
                   className="text-blue-500 ml-2"
                 >
-                  {showFullBiography ? 'Read less' : 'Read more'}
+                  read more
                 </button>
               )}
             </p>
