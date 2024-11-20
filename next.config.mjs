ecb8604717a156c26578a8c74f7f4decc479fa42
Dimitri <dmytro.palahin@gmail.com>
2024-11-20 07:55:23 +0100

    change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['image.tmdb.org'], // Autoriser le domaine pour les images
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'image.tmdb.org',
+                pathname: '/**',
+            },
+        ],
     },
-  };
-  
-  export default nextConfig;
+};
+
+export default nextConfig;
