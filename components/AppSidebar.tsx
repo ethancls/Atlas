adecc63699e5e718b292806953e82bb8674ce998
ethancls <ethancls@icloud.com>
2024-11-21 00:46:33 +0100

    update

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -1,4 +1,4 @@
-import icon from "@/assets/movie.png"
+import icon from "@/public/movie.png"
 import { Atom, LoaderPinwheelIcon, LogOut, MedalIcon, PlayIcon, StarIcon, TrophyIcon } from "lucide-react"
 import Image from "next/image"
 import Link from 'next/link';
