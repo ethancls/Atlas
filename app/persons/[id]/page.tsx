10b702172468a20dc429467c62c4fef2168bc1a3
Dimitri <dmytro.palahin@gmail.com>
2024-11-23 16:47:01 +0100

    almost finish scroll to movie

--- app/persons/[id]/page.tsx
+++ app/persons/[id]/page.tsx
@@ -256,6 +256,7 @@ const PersonDetailPage = ({ params }: { params: { id: string } }) => {
               onClick={scrollMoviesLeft}
               className="bg-gray-100 dark:bg-[rgb(24,24,27)] p-2 rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300"
               aria-label="Scroll left"
+              style={{ scrollSnapAlign: 'start' }}
             >
               <ChevronLeftIcon className="h-7 w-7 text-gray-400" />
             </button>
@@ -270,9 +271,9 @@ const PersonDetailPage = ({ params }: { params: { id: string } }) => {
                     <button
                       key={movie.id}
                       onClick={() => router.push(`/movies/${movie.id}`)}
-                      className="flex-shrink-0 w-[200px] md:w-[250px] lg:w-[300px] cursor-pointer hover:opacity-80 snap-center"
+                      className="flex-shrink-0 w-[150px] sm:w-[200px] md:w-[250px] lg:w-[300px] cursor-pointer hover:opacity-80 snap-center"
                       aria-label={`View details for ${movie.title ?? 'Unknown title'}`}
-                      style={{ flex: '0 0 auto' }}
+                      style={{ flex: '0 0 auto', scrollSnapAlign: 'start' }}
                     >
                       <Image
                         src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
@@ -281,6 +282,7 @@ const PersonDetailPage = ({ params }: { params: { id: string } }) => {
                         height={400}
                         className="rounded-md shadow-md"
                         style={{ width: 'auto', height: 'auto' }}
+                        sizes="(min-width:300px) and (max-width:739px) 150px, (min-width:740px) and (max-width:999px) 200px, (min-width:1000px) and (max-width:1319px) 250px, 300px"
                       />
                     </button>
                   )
@@ -290,6 +292,7 @@ const PersonDetailPage = ({ params }: { params: { id: string } }) => {
               onClick={scrollMoviesRight}
               className="bg-gray-100 dark:bg-[rgb(24,24,27)] p-2 rounded-full shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300"
               aria-label="Scroll right"
+              style={{ scrollSnapAlign: 'start' }}
             >
               <ChevronRightIcon className="h-7 w-7 text-gray-400" />
             </button>
